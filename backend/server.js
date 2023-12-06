bcdf4b6268f43c30ee39b2f830411f578d72ad86
sammanamgain <lionalmessi609@gmail.com>
2023-12-06 15:08:19 +0545

    setup

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+app.listen(process.env.PORT, () => {
+    console.log("listening on port " + process.env.port);
+})
