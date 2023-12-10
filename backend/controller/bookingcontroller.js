f64b631ed0e1c2c2148e9d47eca52fef3350ccd3
sammanamgain <lionalmessi609@gmail.com>
2023-12-10 18:28:41 +0545

    completion of current user and changing the routing based on currentuser

---
+++ backend/controller/bookingcontroller.js
@@ -0,0 +1,9 @@
+const User = require("../models/usermodel");
+const Movie = require("../models/movielist.js");
+const catchAsync = require("../middleware/catchAsync");
+const Error = require("../utils/Error");
+const customError = require("../middleware/customError.js");
+
+exports.book = catchAsync(async (req, res, next) => [
+    
+]);
