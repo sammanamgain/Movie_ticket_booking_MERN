aa8363a0b4711ac268e74d528e1219d6fcffdcbf
sammanamgain <lionalmessi609@gmail.com>
2023-12-11 10:56:01 +0545

    fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./src/App.jsx";
 import "./index.css";
 import { persistor, store } from "./redux/store.js";
 import { Provider } from "react-redux";
